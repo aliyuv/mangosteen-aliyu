e688f3a48924771991aad4b5cf125426390091d7
aliyu <1324149139@qq.com>
2023-02-03 17:00:19 +0800

    refactor: 重构main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-// 定义路由
-const routes = [
-    { path: '/', component: Foo },
-    { path: '/bar', component: Bar },
-  ]
-
-// 路由器
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-  })
+const router = createRouter({ history,routes })
   
 const app = createApp(App)
 app.use(router)
