92fb880ad5bfac26734413cf2d53036d953c7651
aliyu <1324149139@qq.com>
2023-02-04 16:07:04 +0800

    fix: 在手机端页面显示不全bug

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,12 +1,13 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import s from "./Welcome.module.scss";
 import mangosteen from "../assets/logo/mangosteen.svg";
+
 export const Welcome = defineComponent({
   setup() {
     return () => (
       <div class={s.wrapper}>
-        <h1><RouterLink to="/start">跳过</RouterLink></h1>
+        <div class={s.skiplink}><RouterLink to="/start">跳过</RouterLink></div>
         <header>
           <img src={mangosteen} />
           <h2>山竹记账</h2>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0"
+  },
   plugins: [
     vue(),
     vueJsx({
