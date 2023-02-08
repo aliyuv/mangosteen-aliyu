d53974b9bba00d46d9fb9acaade8ebd969965fc3
aliyu <1324149139@qq.com>
2023-02-08 23:25:26 +0800

    feature: 完成路由切换动画

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,5 +1,5 @@
-import { defineComponent, ref } from "vue";
-import { RouterLink, RouterView } from "vue-router";
+import { defineComponent, h, Transition, VNode } from "vue";
+import { RouteLocationNormalizedLoaded, RouterLink, RouterView } from "vue-router";
 import s from "./Welcome.module.scss";
 import mangosteen from "../assets/logo/mangosteen.svg";
 import vhCheck from 'vh-check'
@@ -14,7 +14,19 @@ export const Welcome = defineComponent({
           <img src={mangosteen} />
           <h2>山竹记账</h2>
         </header>
-        <main><RouterView /></main>
+        <main>
+          <RouterView>
+            {({Component: x,route: R} : {Component: VNode, route: RouteLocationNormalizedLoaded}) =>
+               <Transition 
+               enterActiveClass={s.slide_fade_enter_active} 
+               leaveActiveClass={s.slide_fade_leave_active} 
+               enterFromClass={s.slide_fade_enter_from} 
+               leaveToClass={s.slide_fade_leave_to}>
+                {x}
+              </Transition>
+            }
+          </RouterView>
+        </main>
       </div>
     </>
   }
