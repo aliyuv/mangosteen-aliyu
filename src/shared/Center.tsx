dab17542aaef672c05beda69ce7f4ae331b26702
aliyu <1324149139@qq.com>
2023-02-10 15:52:26 +0800

    feature: 制作 Center 组件

---
+++ src/shared/Center.tsx
@@ -0,0 +1,25 @@
+import { defineComponent, PropType } from "vue"
+import s from "./Center.module.scss"
+const directionsMap = {
+  "-": "horizontal",
+  "|": "vertical",
+  horizontal: "horizontal",
+  vertical: "vertical"
+}
+export const Center = defineComponent({
+  props: {
+    direction: {
+      type: String as PropType<"-" | "|" | "horizontal" | "vertical">,
+      default: "horizontal"
+    }
+  },
+  setup(props, context) {
+    const extraClass = directionsMap[props.direction]
+    console.log(extraClass)
+    return () => (
+      <div class={[s.center, extraClass]}>
+        {context.slots.default?.()}
+      </div>
+    )
+  }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue"
 import { Button } from "../shared/Button"
+import { Center } from "../shared/Center"
 import { FloatButton } from "../shared/FloatButton"
+import { Icon } from "../shared/Icon"
 import s from "./StartPage.module.scss"
 export const StartPage = defineComponent({
   setup(props, context) {
@@ -9,6 +11,10 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
+        <nav>导航</nav>
+        <Center class={s.pig_wrapper}>
+          <Icon name="pig" class={s.pig} />
+        </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>按钮</Button>
         </div>
