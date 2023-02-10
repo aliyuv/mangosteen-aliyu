ae19dca15463e73571eda1251d4027e514e812d8
aliyu <1324149139@qq.com>
2023-02-10 19:02:28 +0800

    feature: 完善 Overlay 样式

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, PropType } from "vue"
+import { RouterLink } from "vue-router"
 import { Icon } from "./Icon"
 import s from "./Overlay.module.scss"
 export const Overlay = defineComponent({
@@ -12,26 +13,33 @@ export const Overlay = defineComponent({
       props.onClose?.()
       // context.emit("close") // 这里的close是父组件 onclose的函数
     }
+    const onClickSingIn = () => { }
     return () => <>
       <div class={s.make} onClick={close}></div>
       <div class={s.overlay}>
-        <section>
+        <section class={s.currentUser} onClick={onClickSingIn}>
           <h2>未登录用户</h2>
           <p>点击这里登录</p>
         </section>
         <nav>
-          <ul>
+          <ul class={s.action_list}>
             <li>
-              <Icon name="chart" />
-              <span>统计图表</span>
+              <RouterLink to='/statistics' class={s.action}>
+                <Icon name="chart" class={s.icon} />
+                <span>统计图表</span>
+              </RouterLink>
             </li>
             <li>
-              <Icon name="export" />
-              <span>导出数据</span>
+              <RouterLink to='/export' class={s.action}>
+                <Icon name="export" class={s.icon} />
+                <span>导出数据</span>
+              </RouterLink>
             </li>
             <li>
-              <Icon name="notify" />
-              <span>记账提醒</span>
+              <RouterLink to='/notify' class={s.action}>
+                <Icon name="notify" class={s.icon} />
+                <span>记账提醒</span>
+              </RouterLink>
             </li>
           </ul>
         </nav>
