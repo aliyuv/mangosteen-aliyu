83a87ac03896242ce68b841c29f6519640aef148
aliyu <1324149139@qq.com>
2023-02-10 18:50:28 +0800

    feature: 创建 Overlay 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,16 +1,20 @@
 import { defineComponent, PropType } from "vue";
 import s from "./Icon.module.scss";
-export type IconType = "add" | "chart" | "clock" | "cloud" | "mangosteen" | "pig" | "menu";
+export type IconType = "add" | "chart" | "clock" | "cloud" | "mangosteen" | "pig" | "menu" | "charts" | "export" | "notify";
 export const Icon = defineComponent({
   props: {
     name: {
       type: String as PropType<IconType>,
       required: true
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>,
     }
   },
   setup(props, context) {
     return () => (
-      <svg class={s.icon}>
+      // 这里的onClick 如果在props中定义了类型,那么这里就必须要写上onClick,否则会报错
+      <svg class={s.icon} onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`}></use>
       </svg>
     )
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,21 +1,23 @@
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 import { Button } from "../shared/Button"
 import { Center } from "../shared/Center"
 import { FloatButton } from "../shared/FloatButton"
 import { Icon } from "../shared/Icon"
 import { NavBar } from "../shared/NavBar"
+import { Overlay } from "../shared/Overlay"
 import s from "./StartPage.module.scss"
 export const StartPage = defineComponent({
   setup(props, context) {
-    const onClick = () => {
-      console.log("click")
+    const isVisiable = ref(false)
+    const clcikMenu = () => {
+      isVisiable.value = !isVisiable.value
     }
     return () => (
       <div>
         <nav>
           <NavBar>
             {{
-              icon: () => <Icon name="menu" class={s.navIcon} />,
+              icon: () => <Icon name="menu" class={s.navIcon} onClick={clcikMenu} />,
               title: () => <span>山竹记账</span>
             }}
           </NavBar>
@@ -24,9 +26,12 @@ export const StartPage = defineComponent({
           <Icon name="pig" class={s.pig} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>开始记账</Button>
+          <Button class={s.button}>开始记账</Button>
         </div>
         <FloatButton IconName="add" />
+        {
+          isVisiable.value && <Overlay onClose={() => isVisiable.value = false} />
+        }
       </div>
     )
   }
