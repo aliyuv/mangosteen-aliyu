67959e3a3e2a4866f7fcb06a2d5a34f11f973ccf
aliyu <1324149139@qq.com>
2023-02-10 21:05:26 +0800

    wip: /items/create 页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,11 +1,17 @@
 import { defineComponent } from "vue";
+import { MainLayout } from "../../Layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
 import s from "./ItemCreate.module.scss";
 export const ItemCreate = defineComponent({
   setup(props, context) {
     return () => (
-      <div class={s.wrapper}>
-        <h1>ItemCreate</h1>
-      </div>
+      <MainLayout>{{
+        icon: () => <Icon name="left" class={s.navIcon} />,
+        title: () => '记一笔',
+        default: () => <>
+          <div>main</div>
+        </>
+      }}</MainLayout>
     );
   }
 })
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,10 +1,10 @@
 import { defineComponent } from "vue";
-import s from "./ItemPage.module.scss";
+import { RouterView } from "vue-router";
 export const ItemPage = defineComponent({
   setup(props, context) {
     return () => (
-      <div class={s.wrapper}>
-        <h1>Ite1mPage</h1>
+      <div>
+        <RouterView />
       </div>
     );
   }
