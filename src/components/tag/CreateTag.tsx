b3fd32250d97d358f938fe21934040307841972d
aliyu <1324149139@qq.com>
2023-02-13 19:00:58 +0800

    feature: 整理好所有 emoji

--- src/components/tag/CreateTag.tsx
+++ src/components/tag/CreateTag.tsx
@@ -1,200 +1,52 @@
 import { Icon } from '../../shared/Icon'
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import { MainLayout } from '../../Layouts/MainLayout'
 import s from './CreateTag.module.scss'
 import { Button } from '../../shared/Button'
+import { EmojiSelect } from '../../shared/EmojiSelect'
 export const CreateTag = defineComponent({
   setup(props, context) {
+    const formData = reactive({
+      name: '',
+      sign: ''
+    })
     return () => (
-      <div class={s.wrapper}>
-        <MainLayout>{{
-          title: () => '新建标签',
-          icon: () => <Icon name='left' />,
-          default: () => (
-            <form class={s.form}>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>标签名</span>
-                  <div class={s.formItem_value}>
-                    <input class={[s.formItem, s.input, s.error]}></input>
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>必填</span>
-                  </div>
-                </label>
-              </div>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号</span>
-                  <div class={s.formItem_value}>
-                    <div class={[s.formItem, s.emojiList, s.error]}>
-                      <nav>
-                        <span class={s.selected}>表情</span>
-                        <span>手势</span>
-                        <span>职业</span>
-                        <span>衣服</span>
-                        <span>动物</span>
-                        <span>自然</span>
-                        <span>食物</span>
-                        <span>运动</span>
-                        <span>表情</span>
-                        <span>手势</span>
-                        <span>职业</span>
-                        <span>衣服</span>
-                        <span>动物</span>
-                        <span>自然</span>
-                        <span>食物</span>
-                        <span>运动</span>
-                      </nav>
-                      <ol>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                        <li>😀</li>
-                      </ol>
-                    </div>
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>必填</span>
-                  </div>
-                </label>
-              </div>
-              <p class={s.tips}>记账时长按标签即可进行编辑</p>
-              <div class={s.formRow}>
+      <MainLayout>{{
+        title: () => '新建标签',
+        icon: () => <Icon name="left" onClick={() => { }} />,
+        default: () => (
+          <form class={s.form}>
+            <div class={s.formRow}>
+              <label class={s.formLabel}>
+                <span class={s.formItem_name}>标签名</span>
                 <div class={s.formItem_value}>
-                  <Button class={[s.formItem, s.button]}>确定</Button>
+                  <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
                 </div>
+                <div class={s.formItem_errorHint}>
+                  <span>必填</span>
+                </div>
+              </label>
+            </div>
+            <div class={s.formRow}>
+              <label class={s.formLabel}>
+                <span class={s.formItem_name}>符号</span>
+                <div class={s.formItem_value}>
+                  <EmojiSelect class={[s.formItem, s.emojiList, s.error]} />
+                </div>
+                <div class={s.formItem_errorHint}>
+                  <span>必填</span>
+                </div>
+              </label>
+            </div>
+            <p class={s.tips}>记账时长按标签即可进行编辑</p>
+            <div class={s.formRow}>
+              <div class={s.formItem_value}>
+                <Button class={[s.formItem, s.button]}>确定</Button>
               </div>
-            </form>
-          )
-        }}</MainLayout>
-      </div>
+            </div>
+          </form>
+        )
+      }}</MainLayout>
     )
   }
 })
