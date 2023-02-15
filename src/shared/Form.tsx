61d6955692a986e92e38a3bd5f19af6bd8be643e
aliyu <1324149139@qq.com>
2023-02-15 16:00:14 +0800

    fix: 给 v-model 加上对应的 emits

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -33,6 +33,7 @@ export const FormItem = defineComponent({
       type: String as PropType<string>
     }
   },
+  emits: ['update:modelValue'],
   setup: (props, context) => {
     const refDateVisible = ref(false);
     const content = computed(() => {
