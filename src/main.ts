42aaf1a1d649d31aaefff65301a72092f6076e52
aliyu <1324149139@qq.com>
2023-02-18 07:23:34 +0800

    feature: 优化了，但是代码行数变多了

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,30 @@ import { routes } from './config/routes'
 import { history } from './shared/history'
 import "@svgstore" 
 import { fetchMe, mePromise } from './shared/me'
+import { TRUE } from 'sass'
 const router = createRouter({ history,routes })
 fetchMe()
 
+const whiteList: Record<string, 'exact' | 'startWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startWith',
+  '/sign_in': 'startWith',
+
+}
 router.beforeEach(async (to, from) => {
-  if(to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in' || to.path === '/start')){
-    return true 
-  }else {
-    const path = await mePromise?.then( 
-      () => true ,
-      () => '/sign_in?return_to=' + to.path)
-    return path
-  }
+    for(const key in whiteList){
+      const value = whiteList[key]
+      if(value === 'exact' && to.path === key){
+        return true
+      }
+      if(value === 'startWith' && to.path.startsWith(key)){
+        return true
+      }
+    }
+    return mePromise!.then((me) => 
+    () => true,
+    () => '/sign_in?return_to=' + to.path)
 })
 const app = createApp(App)
 app.use(router)
