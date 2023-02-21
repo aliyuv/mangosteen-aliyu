a28e5692a9a4a3a74a87f698adb87b4a2a700e9d
aliyu <1324149139@qq.com>
2023-02-21 15:06:38 +0800

    feature: 封装 Money

---
+++ src/shared/Money.tsx
@@ -0,0 +1,28 @@
+import { defineComponent, PropType } from "vue";
+
+export const Money = defineComponent({
+  props: {
+    value: {
+      type: Number as PropType<number>,
+      required: true,
+    }
+  },
+  setup(props, context) {
+    const addZero = (num: number) => {
+      const nString = num.toString()
+      const doIndex = nString.indexOf('.')
+      if (doIndex < 0) { // 没有小数点
+        return nString + '.00'
+      } else if (nString.substring(doIndex).length === 2) { // 小数点后面只有一位
+        return nString + '0'
+      } else {
+        return nString
+      }
+    }
+    return () => (
+      <span>
+        {addZero(props.value / 100)}
+      </span>
+    )
+  }
+})
