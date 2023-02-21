3f061a91930df5727aaff4e6714e59613491e2cb
aliyu <1324149139@qq.com>
2023-02-21 14:38:18 +0800

    feature: 自定义时间默认为空，不发请求

--- src/Layouts/TimeTabsLayOut.tsx
+++ src/Layouts/TimeTabsLayOut.tsx
@@ -10,11 +10,11 @@ const demo = defineComponent({
   props: {
     startDate: {
       type: String as PropType<string>,
-      required: true,
+      required: false,
     },
     endDate: {
       type: String as PropType<string>,
-      required: true,
+      required: false,
     },
   },
 })
@@ -28,10 +28,10 @@ export const TimeTabsLayOut = defineComponent({
   setup(props, context) {
     const refSelected = ref('本月')
     const time = new Time()
-    const customTime = reactive({
-      start: new Time().format(),
-      end: new Time().format(),
-    })
+    const customTime = reactive<{
+      start?: string
+      end?: string
+    }>({})
     const timeList = [
       {
         start: time.firstDayOfMonth(),
