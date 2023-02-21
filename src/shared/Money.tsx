86485405598fe5d4409b85bfc334c81c7c2f2010
aliyu <1324149139@qq.com>
2023-02-21 17:02:59 +0800

    feature: 美化折线图

--- src/shared/Money.tsx
+++ src/shared/Money.tsx
@@ -8,19 +8,23 @@ export const Money = defineComponent({
     },
   },
   setup(props, context) {
-    const addZero = (num: number) => {
-      const nString = num.toString()
-      const doIndex = nString.indexOf('.')
-      if (doIndex < 0) {
-        // 没有小数点
-        return nString + '.00'
-      } else if (nString.substring(doIndex).length === 2) {
-        // 小数点后面只有一位
-        return nString + '0'
-      } else {
-        return nString
-      }
-    }
     return () => <span>{addZero(props.value / 100)}</span>
   },
 })
+
+const addZero = (num: number) => {
+  const nString = num.toString()
+  const doIndex = nString.indexOf('.')
+  if (doIndex < 0) {
+    // 没有小数点
+    return nString + '.00'
+  } else if (nString.substring(doIndex).length === 2) {
+    // 小数点后面只有一位
+    return nString + '0'
+  } else {
+    return nString
+  }
+}
+export const getMoney = (value: number) => {
+  return addZero(value / 100)
+}
