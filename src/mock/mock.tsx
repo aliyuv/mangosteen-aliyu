e79a3db902b91468b90a2b43abcf4cf9ccd6e2c9
aliyu <1324149139@qq.com>
2023-02-21 19:36:44 +0800

    feature: 数据修剪，改补零的补零

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -9,9 +9,9 @@ export const mockItemSummary: Mock = (config) => {
     200,
     {
       groups: [
-        { happen_at: '2018-06-18T00:00:00.000+0800', amount: 100 },
-        { happen_at: '2018-06-22T00:00:00.000+0800', amount: 300 },
-        { happen_at: '2018-06-29T00:00:00.000+0800', amount: 200 }
+        { happen_at: '2023-02-10T00:00:00.000+0800', amount: 100 },
+        { happen_at: '2023-02-15T00:00:00.000+0800', amount: 300 },
+        { happen_at: '2023-02-21T00:00:00.000+0800', amount: 200 }
       ],
       summary: 600
     }
