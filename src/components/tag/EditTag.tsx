b3e6f19fdd11dd3e785854e8ecedb8fae23bde66
aliyu <1324149139@qq.com>
2023-02-22 23:49:59 +0800

    feature: tag删除时支持 with_items 选项

--- src/components/tag/EditTag.tsx
+++ src/components/tag/EditTag.tsx
@@ -28,7 +28,7 @@ export const EditTag = defineComponent({
       })
       await http
         .delete(`/tags/${numberId}`, {
-          withItems: options?.withItems ? 'true' : 'false'
+          with_items: options?.withItems ? 'true' : 'false'
         }, { _autoLoading: true })
         .catch(onError)
       router.back()
