084d0ac6a52b4cf51428c267f5700af2b8d8b3e2
aliyu <1324149139@qq.com>
2023-02-23 21:59:38 +0800

    fix: 修复错误地请求了下一页 items 的 bug

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -31,23 +31,20 @@ export const ItemCreate = defineComponent({
     }
     const onSubmit = async () => {
       Object.assign(errors, { kind: [], tag_ids: [], amount: [], happen_at: [] })
-      Object.assign(
-        errors,
-        validate(formData, [
-          { key: 'kind', type: 'required', message: '类型必填' },
-          { key: 'tag_ids', type: 'required', message: '标签必填' },
-          { key: 'amount', type: 'required', message: '金额必填' },
-          { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为零' },
-          { key: 'happen_at', type: 'required', message: '时间必填' }
-        ])
-      )
+      Object.assign(errors, validate(formData, [
+        { key: 'kind', type: 'required', message: '类型必填' },
+        { key: 'tag_ids', type: 'required', message: '标签必填' },
+        { key: 'amount', type: 'required', message: '金额必填' },
+        { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为零' },
+        { key: 'happen_at', type: 'required', message: '时间必填' },
+      ]))
       if (hasError(errors)) {
+        debugger
         Dialog.alert({
           title: '出错',
-          message: Object.values(errors)
-            .filter((i) => i.length > 0)
-            .join('\n')
+          message: Object.values(errors).filter(i => i.length > 0).join('\n')
         })
+
         return
       }
       await http.post<Rescource<Item>>('/items', formData, { _mock: 'itemCreate', _autoLoading: true }).catch(onError)
--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -4,7 +4,11 @@ interface FData {
 type Rule<T> = {
   key: keyof T
   message: string
-} & ({ type: 'required' } | { type: 'pattern'; regex: RegExp } | { type: 'notEqual'; value: JSONValue })
+} & (
+    { type: 'required' } |
+    { type: 'pattern', regex: RegExp } |
+    { type: 'notEqual', value: JSONValue }
+  )
 type Rules<T> = Rule<T>[]
 export type { Rules, Rule, FData }
 export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
@@ -12,7 +16,7 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
     [k in keyof T]?: string[]
   }
   const errors: Errors = {}
-  rules.map((rule) => {
+  rules.map(rule => {
     const { key, type, message } = rule
     const value = formData[key]
     switch (type) {
@@ -21,19 +25,19 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
           errors[key] = errors[key] ?? []
           errors[key]?.push(message)
         }
-        break
+        break;
       case 'pattern':
         if (!isEmpty(value) && !rule.regex.test(value!.toString())) {
           errors[key] = errors[key] ?? []
           errors[key]?.push(message)
         }
-        break
+        break;
       case 'notEqual':
         if (!isEmpty(value) && value === rule.value) {
           errors[key] = errors[key] ?? []
           errors[key]?.push(message)
         }
-        break
+        break;
       default:
         return
     }
@@ -46,6 +50,8 @@ function isEmpty(value: null | undefined | string | number | FData) {
 }
 
 export function hasError(errors: Record<string, string[]>) {
+  // return Object.values(errors)
+  // .reduce((result, value) => result + value.length, 0) > 0
   let result = false
   for (let key in errors) {
     if (errors[key]?.length > 0) {
