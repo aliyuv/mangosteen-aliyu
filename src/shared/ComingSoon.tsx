2bd296c6f54718acee0b8ddc99bf6a7b62e4d02c
aliyu <1324149139@qq.com>
2023-02-24 17:31:43 +0800

    feature: 敬请期待添加返回按钮

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,15 +1,24 @@
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { Button } from './Button'
 import { Center } from './Center'
 import s from './ComingSoon.module.scss'
 import { Icon } from './Icon'
 export const ComingSoon = defineComponent({
   setup(props, context) {
+    const router = useRouter()
+    const onClick = () => {
+      router.back()
+    }
     return () => (
-      <div>
+      <div class={s.wrapper}>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig} />
         </Center>
         <p class={s.text}>敬请期待</p>
+        <div class={s.link}>
+          <Button class={s.backButton} onClick={onClick}><Icon name='left' /></Button>
+        </div>
       </div>
     )
   }
