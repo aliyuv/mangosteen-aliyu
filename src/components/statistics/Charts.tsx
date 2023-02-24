b5440cf3bf453fa41d610442d1b6bedf1c63588c
aliyu <1324149139@qq.com>
2023-02-24 17:08:04 +0800

    fix: 前端默认格式不全的时间是北京时间

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -36,7 +36,8 @@ export const Charts = defineComponent({
       return Array.from({ length: n }).map((_, i) => {
         const time = new Time(`${props.startDate}T00:00:00.000+0800`).add(i, 'day').getTimestamp()
         const item = data1.value[0]
-        const amount = item && new Date(item.happen_at).getTime() === time ? data1.value.shift()!.amount : 0
+        // 服务器返回的数据就是按照 中国北京时区的，所以这里需要加上 8 小时
+        const amount = item && new Date(item.happen_at + 'T00:00:00.000+0800').getTime() === time ? data1.value.shift()!.amount : 0
         return [new Date(time).toISOString(), amount]
       })
     })
